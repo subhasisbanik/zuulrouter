abf8dcb11fb2670e89738b9a00bcf0a64d2f1436
Subhasis Banik <subhasisbanik@gmail.com>
2020-12-03 18:24:08 +0400

    baseline code for zuul gateway router

---
+++ src/test/java/com/zuulrouter/ZuulRouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuulrouter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulRouterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
